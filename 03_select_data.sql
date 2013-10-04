625e49a1b763e6dfe1b9d0002239bc8de8fd8984
ivanbrennan <ivan.brennan@gmail.com>
2013-10-04 10:37:52 -0400

    Fix selects

--- 03_select_data.sql
+++ 03_select_data.sql
@@ -1,47 +1,30 @@
 -- Select the titles of all projects and their pledge amounts.
-SELECT title, amount FROM(
-  SELECT * FROM projects
-  INNER JOIN pledges
-  ON projects.id = pledges.projects_id
-);
+SELECT title, amount
+FROM projects INNER JOIN pledges ON projects.id = projects_id
+GROUP BY projects_id;
 
 -- Select the user name, age, and pledge amount for all pledges.
-SELECT name, age, amount FROM(
-  SELECT * FROM pledges
-  INNER JOIN users
-  ON pledges.users_id = users.id
-);
+SELECT name, age, amount
+FROM users INNER JOIN pledges ON users.id = users_id;
 
 -- Select the titles of all projects that have met their funding goal.
--- Not working yet.
-SELECT title FROM(
-  SELECT * FROM projects
-  INNER JOIN pledges
-  ON projects.id = pledges.projects_id
-)
-WHERE sum(amount) >= funding_goal;
+SELECT title
+FROM projects INNER JOIN pledges ON projects.id = projects_id
+
+GROUP BY projects_id
+HAVING SUM(amount) >= funding_goal;
 
 -- Select user names and amounts of all pledges. Order them by the amount.
--- Not sorted by amount yet.
-SELECT name, amount FROM(
-  SELECT * FROM pledges
-  INNER JOIN users
-  ON pledges.users_id = users.id
-);
+SELECT name, amount
+FROM pledges INNER JOIN users ON pledges.users_id = users.id
+ORDER BY amount;
 
 -- Select the category names, and pledge amounts of all pledges in the music category.
-SELECT title, amount FROM(
-  SELECT * FROM pledges
-  INNER JOIN projects
-  ON pledges.projects_id = projects.id
-)
+SELECT title, amount
+FROM pledges INNER JOIN projects ON pledges.projects_id = projects.id
 WHERE category = 'music';
 
 -- Select the category names and the sum total of the pledge amounts of all the pledges in the book category.
--- Not working yet.
-SELECT category, sum(amount) FROM(
-  SELECT * FROM pledges
-  INNER JOIN projects
-  ON pledges.projects_id = projects.id
-)
+SELECT category, SUM(amount)
+FROM pledges INNER JOIN projects ON pledges.projects_id = projects.id
 WHERE category = 'books';
